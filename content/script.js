4dad22bf273d6bcd9dea3f4f3c350991d8305a1e
Cinco24 <cincadzelasha24@gmail.com>
2024-06-10 20:25:57 +0400

    nuzarmacobt bijo gavakete content da main page

--- content/script.js
+++ content/script.js
@@ -1,3 +1,14 @@
 const mopen = () => document.getElementById("manager-list").classList.toggle("hidden");
 
-const popen = () => document.getElementById("player-list").classList.toggle("hidden");
+const topen = () => document.getElementById("team-list").classList.toggle("hidden");
+
+const Aopen = () => document.getElementById("attacker-list").classList.toggle("hidden");
+
+const midfopen = () => document.getElementById("midfielder-list").classList.toggle("hidden");
+
+const dopen = () => document.getElementById("deffender-list").classList.toggle("hidden");
+
+const gopen = () => document.getElementById("goalkeeper-list").classList.toggle("hidden");
+
+
+
